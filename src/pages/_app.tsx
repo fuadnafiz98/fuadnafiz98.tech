d2031ea4191910931d8d420dee68ecdf0d44f0da
fuadnafiz98 <fuadnafiz98@gmail.com>
2021-08-19 23:47:50 +0600

    feat: analytis added

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,25 @@
 import { AppProps } from "next/app";
+import Script from "next/script";
 import "@/styles/global.css";
 
 import Base from "../layouts/base";
 
 export default function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Base>
-      <Component {...pageProps} />
-    </Base>
+    <>
+      <Script strategy="lazyOnload" src="https://www.googletagmanager.com/gtag/js?id=G-K60TPVBN7T" />
+      <Script strategy="lazyOnload">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-K60TPVBN7T');
+       `}
+      </Script>
+      <Base>
+        <Component {...pageProps} />
+      </Base>
+    </>
   );
 }
